94188bc8cea4f4a6a5faddb557df6297ec376955
Erik De Lamarter <erik@delamarter.ch>
2017-12-16 15:30:01 +0100

    switch gpg key server

--- add-cgi-builds.sh
+++ add-cgi-builds.sh
@@ -4,5 +4,8 @@ for d in */*/; do
     echo "updating ${d}cgi"
     rm -rf "${d}cgi"
     cp -r "${d}cli" "${d}cgi"
+    # remove the disable-cgi flag
     sed -i '' '/--disable-cgi/s/^/#/g' "${d}cgi/Dockerfile"
+    # change keyserver for GPG
+    sed -i '' 's/ha.pool.sks-keyservers.net/pgp.mit.edu/g' "${d}cgi/Dockerfile"
 done
